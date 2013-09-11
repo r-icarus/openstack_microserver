e5f73ce9263a271acddd53ea93759dbb45a1ea43
Ricardo Echavarria <rgechavarria@gmail.com>
2013-09-11 12:42:11 -0600

    Making the list dynamic

--- openstack_dashboard/dashboards/microserver/recipes/views.py
+++ openstack_dashboard/dashboards/microserver/recipes/views.py
@@ -5,6 +5,7 @@ class IndexView(views.APIView):
     # A very simple class-based view...
     template_name = 'microserver/recipes/index.html'
 
-    def get_data(self, request, context, *args, **kwargs):
+    def get_data(self, request, context, *args, **kwargs):    	
         # Add data to the context here...
+        context["recipe_types"] = ["Blog","Mail Server", "Wiki", "Project Management"]
         return context
